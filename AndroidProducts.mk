91257e05c248e3b047664655be1488a8f2422905
chuqol <151944573+chuqol@users.noreply.github.com>
2023-12-27 11:38:13 +0100

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#               2022-2023 The Evolution X Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/evolution_kebab.mk
+
+COMMON_LUNCH_CHOICES := \
+    evolution_kebab-user \
+    evolution_kebab-userdebug \
+    evolution_kebab-eng
---
+++ evolution_kebab.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#               2022-2023 The Evolution X Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from kebab device
+$(call inherit-product, device/oneplus/kebab/device.mk)
+
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := evolution_kebab
+PRODUCT_DEVICE := kebab
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := KB2005
+
+PRODUCT_SYSTEM_NAME := OnePlus8T
+PRODUCT_SYSTEM_DEVICE := OnePlus8T
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="OnePlus8T-user 13 RKQ1.211119.001 R.13ebe2e_1-170dfb release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := OnePlus/OnePlus8T/OnePlus8T:13/RKQ1.211119.001/R.13ebe2e_1-170dfb:user/release-keys
